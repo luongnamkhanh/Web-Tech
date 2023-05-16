8d5efa4c62f76ef9994ee96187fea14d4451bb73
Minh Huyen <huyen.nm206652@sis.hust.edu.vn>
2023-05-16 15:51:08 +0700

    abc

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './HomePage.css';
+import GameImage from './GameImage/GameImage';
+import GameMenu from './GameMenu/GameMenu';
 
-const HomePage = () => {
-  const handleStartGame = () => {
-    // Logic for starting game
-  }
-
+function HomePage({handleStartGame}) {
   return (
     <div className="container">
-      <div className="image-container">
-        <img className="chess-image" src="img/chess2.png" alt="chess" />
-      </div>
-      <div className="menu-game">
-        <h1 className="animate__animated animate__fadeIn chess-title">Chess</h1>
-        <Link className="startGame button animate__animated animate__fadeIn animate__delay-1s element-title" onClick={handleStartGame}>
-          Start A New Game
-        </Link>
-        <br />
-        <Link className="highScore button animate__animated animate__fadeIn animate__delay-1s element-title" to="/highScores">
-          High Scores
-        </Link>
-      </div>
+      <GameImage />
+      <GameMenu handleStartGame={handleStartGame} />
     </div>
   );
 }
